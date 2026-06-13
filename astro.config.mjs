d0c1bf290c57ace9c5e9b8fc1658b737ff1a10e6
domljl <domljl07@gmail.com>
2026-06-14 01:56:52 +0800

    added EmailJS and Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
     build: {
